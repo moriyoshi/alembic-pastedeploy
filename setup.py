3301e0cb6299e11f01a1db483b3b42a0fcbca89c
Moriyoshi Koizumi <mozo@mozo.jp>
2016-08-05 10:28:48 +0900

    Bump bump...

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 long_description = open('README.rst').read()
 
 setup(name='alembic_pastedeploy',
-      version='0.0.1',
+      version='0.0.2',
       description="Let alembic read pastedeploy-flavored ini config files.",
       long_description=long_description,
       classifiers=[
