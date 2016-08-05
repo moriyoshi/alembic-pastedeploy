2ac75e32bb770c6c216a18f247826ca327c1c52b
Moriyoshi Koizumi <mozo@mozo.jp>
2016-08-05 10:17:53 +0900

    long_description

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-long_description = ''
+long_description = open('README.rst').read()
 
 setup(name='alembic_pastedeploy',
       version='0.0.1dev0',
